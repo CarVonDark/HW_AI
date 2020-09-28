ff883686a3dca81ed4b31f6a817a530fa7fb2480
Tingfei Carlos Feng <2097608048@qq.com>
2020-09-28 10:46:56 -0400

    parse done

--- Projects/NN/src/FeedForwardNetwork.java
+++ Projects/NN/src/FeedForwardNetwork.java
@@ -11,7 +11,6 @@ public class FeedForwardNetwork {
 	private int numHiddenLayers = 0;
 	private int outputLayerSize = 0;
 	private int maxLayerSize = 0;
-	private static double threshold = 0.01;
 	
 	public FeedForwardNetwork(int inputLayerSize, int hiddenLayerSize, int numHiddenLayers, int outputLayerSize){
 		if (inputLayerSize <= 0) {
@@ -48,7 +47,8 @@ public class FeedForwardNetwork {
 	private double[][][] weights; // [layer][from][to]
 
 	
-	public void initNetwork(double[][] inputs, double[][] desiredOutput, double learningRate, double minWeight, double maxWeight) {
+	@SuppressWarnings("hiding")
+	public void initNetwork(double[][] inputs, double[][] desiredOutput, double learningRate, double initialWeightOffset) {
 		this.trainingSetSize = inputs.length;
 		if (this.trainingSetSize == 0) {
 			System.out.println("No training data.");
@@ -61,23 +61,26 @@ public class FeedForwardNetwork {
 		this.inputs = inputs; 
 		this.desiredOutput = desiredOutput;
 		this.learningRate = learningRate;
-		double weightRange = maxWeight - minWeight;
 		
 		// To simplify the code, we generate a slightly oversized weight matrix. 
 		// Not to worry though, we will only use what we need.
 		this.weights = new double[this.numHiddenLayers+1][this.maxLayerSize][this.maxLayerSize]; 
+
 		for (int l = 0; l < this.numHiddenLayers+1; l++) {
 			for (int i = 0; i < this.hiddenLayerSize; i++){
 				for (int j = 0; j < this.hiddenLayerSize; j++){
-					this.weights[l][i][j] = minWeight + Math.random()*weightRange;
+					this.weights[l][i][j] = Math.random() - initialWeightOffset; 	
 				}
 			}
 		}
 	}
 
-	private static double stepActivationFunction(double input){
-		if (input >= threshold) return 1;
-		return 0;
+	private static double sigmoidActivationFunction(double input){
+		return 1.0 / (1 + Math.exp(-1.0 * input));
+	}
+	
+	private static double sigmoidDerivative(double value){
+		return value * (1 - value);
 	}
 	
 	private void feedForward (double[][] activation, int fromLayerSize, int toLayerSize, int l){
@@ -88,10 +91,97 @@ public class FeedForwardNetwork {
 				inJ += this.weights[l][i][j] * activation[l][i];
 			}
 			// 0 is the first hidden layer
-			activation[l+1][j] = stepActivationFunction(inJ);
+			activation[l+1][j] = sigmoidActivationFunction(inJ);
 		}
 	}
 	
+	public void trainNetwork(int iterations, boolean verbose){
+		for (int k = 0; k < iterations; k++){
+			double globalError = 0.0;
+			//Run through entire training set once.
+			for (int example = 0; example < this.trainingSetSize; example++){
+			    // Used for progress bar. Uncomment if progress bar is desired. 
+				 long startTime = System.currentTimeMillis();
+				
+				// We store the activation of each node (over all input and hidden layers) because we need that data during back propagation.
+				// Please notice that in order to simplify the code, we store the activation of the input layer too.
+				double[][] activation = new double[this.numHiddenLayers+2][this.maxLayerSize];
+				// Reading in the activation from the training data example
+				for (int i = 0; i < this.inputLayerSize; i++){
+					activation[0][i] = this.inputs[example][i];
+				}
+
+				// There may be different sizes for the input, hidden and output layers, hence there are three different calls for feedForeard
+				// input to first hidden layer
+				feedForward(activation, this.inputLayerSize, this.hiddenLayerSize, 0);
+				// hidden to hidden layers
+				for (int l = 1; l < this.numHiddenLayers; l++){
+					feedForward(activation, this.hiddenLayerSize, this.hiddenLayerSize, l);					
+				}
+				// last hidden layer to output layer
+				feedForward(activation, this.hiddenLayerSize, this.outputLayerSize, this.numHiddenLayers);
+
+				
+				// calculating errors
+				double[][] error = new double[this.numHiddenLayers+1][this.maxLayerSize];
+				// Calculating errors at output layer
+				for (int j = 0; j < this.outputLayerSize; j++) {					
+					error[this.numHiddenLayers][j] = sigmoidDerivative(activation[this.numHiddenLayers+1][j]) * (this.desiredOutput[example][j] - activation[this.numHiddenLayers+1][j]);
+					globalError += error[this.numHiddenLayers][j]*error[this.numHiddenLayers][j];
+				}
+				// Calculating error of last hidden layer. 
+				calculateError(error, activation, this.hiddenLayerSize, this.outputLayerSize, this.numHiddenLayers-1);
+				
+				// Calculating error of remaining hidden layers. 
+				for (int l = this.numHiddenLayers - 2; l >= 0; l--){				
+					calculateError(error, activation, this.hiddenLayerSize, this.outputLayerSize, l);
+				}				
+
+				/** Adjusting weights **/ 
+				
+				//adjusting weights at output layer
+				adjustWeights(activation, error, this.hiddenLayerSize, this.outputLayerSize, this.numHiddenLayers);
+				// Adjusting weights at hidden layers.
+				for (int l = this.numHiddenLayers-1; l > 0; l--){
+					adjustWeights(activation, error, this.hiddenLayerSize, this.hiddenLayerSize, l);
+				}
+				// Adjusting weights at input layer.
+				adjustWeights(activation, error, this.inputLayerSize, this.hiddenLayerSize, 0);
+				// Uncomment the following line for a progress bar while training. Does not work in ecplise but does on the command line.
+				if (verbose) {
+					 printProgress(startTime, this.trainingSetSize, (example+1));
+				}
+			}
+			if (verbose) {
+				System.out.println("Completed iteration " + (k+1) +" out of "+ iterations + " " + round((((double)(k+1)/iterations)), 4) + "% Complete");
+				// calculating RMS
+				System.out.println("Global error: " + Math.sqrt(globalError/(this.trainingSetSize*this.outputLayerSize)));
+			}
+		}
+	}
+	
+	private void calculateError(double[][] error, double[][] activation, int fromLayerSize, int toLayerSize, int l){
+		double e;
+		for (int i = 0; i < fromLayerSize; i++) {
+			e = 0;
+			for (int j = 0; j < toLayerSize; j++){
+				e += this.weights[l+1][i][j] * error[l+1][j];
+			}
+			// Recall that activation is off by one, due to activation of input layer.
+			error[l][i] = sigmoidDerivative(activation[l+1][i]) * e;
+		}
+	}
+	
+	private void adjustWeights(double[][] activation, double[][] error, int fromLayerSize, int toLayerSize, int l){
+		for (int i = 0; i < fromLayerSize; i++){
+			for (int j = 0; j < toLayerSize; j++){
+				this.weights[l][i][j] += this.learningRate * activation[l][i] * error[l][j];
+			}
+		}		
+	}
+
+
+	@SuppressWarnings("boxing")
 	public void printWeights(){
 		System.out.println("Input layer.");		
 		for (int i = 0; i < this.inputLayerSize; i++){
@@ -101,7 +191,7 @@ public class FeedForwardNetwork {
 		}
 		
 		System.out.println("\nHidden layers.");
-		for (int l = 1; l < this.numHiddenLayers; l++) {
+		for (int l = 0; l < this.numHiddenLayers-1; l++) {
 			for (int i = 0; i < this.hiddenLayerSize; i++){
 				for (int j = 0; j < this.hiddenLayerSize; j++){
 					System.out.printf("Weight at hidden layer %d from node %d to node %d is %f.\n", l, i, j, this.weights[l][i][j]);
@@ -112,7 +202,7 @@ public class FeedForwardNetwork {
 		System.out.println("\nOutput layer.");
 		for (int i = 0; i < this.hiddenLayerSize; i++){
 			for (int j = 0; j < this.outputLayerSize; j++){
-				System.out.printf("Weight from last hidden layer node %d to output node %d is %f.\n", i, j, this.weights[this.numHiddenLayers][i][j]);
+				System.out.printf("Weight from node %d to output node %d is %f.\n", i, j, this.weights[this.numHiddenLayers][i][j]);
 			}
 		}
 }
@@ -150,6 +240,106 @@ public class FeedForwardNetwork {
 		System.out.println("Done testing.");
 	}
 	
+	/***
+	 * Allows a trained network to be tested on a supplied test batch.
+	 *
+	 * @param testingSetSize - size of testing set.
+	 * @param test_inputs - the actual testing set.
+	 * @param test_labels - the testing sets abels.
+	 * @param verbose - displays information regarding failures.
+	 */
+	public void testNetworkBatch(int testingSetSize, double[][] test_inputs, double[][] test_labels, boolean verbose) {
+		// Used for progress bar. Uncomment if progress bar is desired. 
+		 long startTime;
+		int error_count = 0;
+		//Run through entire training set once.
+		for (int example = 0; example < testingSetSize; example++){
+		    // Used for progress bar. Uncomment is progress bar is desired. 
+			startTime = System.currentTimeMillis();
+			
+			// We store the activation of each node (over all input and hidden layers) because we need that data during back propagation.
+			// Please notice that in order to simplify the code, we store the activation of the input layer too.
+			double[][] activation = new double[this.numHiddenLayers+2][this.maxLayerSize];
+			// Reading in the activation from the training data example
+			for (int i = 0; i < this.inputLayerSize; i++){
+				activation[0][i] = test_inputs[example][i];
+			}
+	
+			// There may be different sizes for the input, hidden and output layers, hence there are three different calls for feedForeard
+			// input to first hidden layer
+			feedForward(activation, this.inputLayerSize, this.hiddenLayerSize, 0);
+			// hidden to hidden layers
+			for (int l = 1; l < this.numHiddenLayers; l++){
+				feedForward(activation, this.hiddenLayerSize, this.hiddenLayerSize, l);					
+			}
+			// last hidden layer to output layer
+			feedForward(activation, this.hiddenLayerSize, this.outputLayerSize, this.numHiddenLayers);
+	
+			for (int j = 0; j < this.outputLayerSize; j++){
+				if (Math.abs(activation[this.numHiddenLayers+1][j] - test_labels[example][j]) > 0.1) {
+					error_count++;
+					if (verbose) {
+						System.out.println("Output Layer: " + round(activation[this.numHiddenLayers+1][0], 2) + ", " + round(activation[this.numHiddenLayers+1][1], 2) + ", " +
+							round(activation[this.numHiddenLayers+1][2], 2) + ", " + round(activation[this.numHiddenLayers+1][3], 2) + ", " +  round(activation[this.numHiddenLayers+1][4], 2) + ", " + 
+							round(activation[this.numHiddenLayers+1][5], 2) + ", " + round(activation[this.numHiddenLayers+1][6], 2) + ", " + round(activation[this.numHiddenLayers+1][7], 2) + ", " + 
+							round(activation[this.numHiddenLayers+1][8], 2) + ", " + round(activation[this.numHiddenLayers+1][9], 2));
+						System.out.println("Label Layer: " + test_labels[example][0] + ", " + test_labels[example][1] + ", " +
+							test_labels[example][2] + ", " + test_labels[example][3] + ", " +  test_labels[example][4] + ", " + 
+							test_labels[example][5] + ", " + test_labels[example][6] + ", " + test_labels[example][7] + ", " + 
+							test_labels[example][8] + ", " + test_labels[example][9]);
+					}
 
+				}
+			}
+			// Uncomment if progress bar is desired. 
+//			if (verbose) {
+//				printProgress(startTime, example, testingSetSize);
+//			}
+		}
+		System.out.println(error_count + " errors");
+		System.out.println("Done testing.");
+		System.out.println("Number correct: " + (testingSetSize - error_count) + " out of: " + testingSetSize);
+		System.out.println("Overall accuracy: " +   round((((double)(testingSetSize-error_count)/testingSetSize)), 4));
+	}
+	
+	public static double round(double value, int places) {
+	    if (places < 0) {
+	    	throw new IllegalArgumentException();
+	    }
+	    
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
+	}
+	// Method works well on console winows running through the command line
+	// however this method does not work well with the Eclipse console window.
+	@SuppressWarnings({ "boxing", "unused" })
+	private static void printProgress(long startTime, long total, long current) {
+	    long eta = current == 0 ? 0 : 
+	        (total - current) * (System.currentTimeMillis() - startTime) / current;
+
+	    String etaHms = current == 0 ? "N/A" : 
+	            String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(eta),
+	                    TimeUnit.MILLISECONDS.toMinutes(eta) % TimeUnit.HOURS.toMinutes(1),
+	                    TimeUnit.MILLISECONDS.toSeconds(eta) % TimeUnit.MINUTES.toSeconds(1));
+
+	    StringBuilder string = new StringBuilder(140);   
+	    int percent = (int) (current * 100 / total);
+	    string
+	        .append('\r')
+	        .append(String.join("", Collections.nCopies(percent == 0 ? 2 : 2 - (int) (Math.log10(percent)), " ")))
+	        .append(String.format(" %d%% [", percent))
+	        .append(String.join("", Collections.nCopies(percent, "=")))
+	        .append('>')
+	        .append(String.join("", Collections.nCopies(100 - percent, " ")))
+	        .append(']')
+	        .append(String.join("", Collections.nCopies(current == 0 ? (int) (Math.log10(total)) : (int) (Math.log10(total)) - (int) (Math.log10(current)), " ")))
+	        .append(String.format(" %d/%d, ETA: %s", current, total, etaHms));
+
+	    System.out.print(string);
+	    if (current == total) {
+	    	System.out.println();
+	    }
+	}
 
 }
--- Projects/NN/src/Training.java
+++ Projects/NN/src/Training.java
@@ -1,18 +1,66 @@
-public class Training {
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-	public static void main(String[] args){
-		FeedForwardNetwork n = new FeedForwardNetwork(2, 3, 1, 1);
-		double[][] inputs = {		
-				{0,0},
-				{0,1},
-				{1,0},
-				{1,1}
-		}; 
-		double[][] desiredOutput = {{0},{1},{1},{0}};		
-		n.initNetwork(inputs, desiredOutput, 0.3, 0, 10);
-		//n.trainNetwork(10000, false);
+public class Training {
+	
+	private static double[][] trainingDataSet;
+	private static double[][] desiredOutput;
+	private static int magicNumber = 0;
+	private static int numberOfItems = 0;
+	private static int nRows = 0;
+	private static int nCols = 0;
+
+	public static void main(String[] args) {
+		
+		try {
+			readData();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		FeedForwardNetwork n = new FeedForwardNetwork(nRows*nCols, nRows, 1, 1);
+		n.initNetwork(trainingDataSet, desiredOutput, 0.2, 1);
+		n.trainNetwork(1000, true);
 		n.printWeights();
 		n.testNetwork();
 	}
 	
+	public static void readData() throws IOException{
+		DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream("images.idx3-ubyte")));
+        magicNumber = dataInputStream.readInt();
+        numberOfItems = dataInputStream.readInt();
+        nRows = dataInputStream.readInt();
+        nCols = dataInputStream.readInt();
+
+        System.out.println("magic number is " + magicNumber);
+        System.out.println("number of items is " + numberOfItems);
+        System.out.println("number of rows is: " + nRows);
+        System.out.println("number of cols is: " + nCols);
+
+        DataInputStream labelInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream("labels.idx1-ubyte")));
+        int labelMagicNumber = labelInputStream.readInt();
+        int numberOfLabels = labelInputStream.readInt();
+
+        System.out.println("labels magic number is: " + labelMagicNumber);
+        System.out.println("number of labels is: " + numberOfLabels);
+        
+        int pixelNum = nRows * nCols;
+        trainingDataSet = new double[numberOfItems][pixelNum];
+        desiredOutput = new double[numberOfLabels][1];
+        
+        for(int i = 0; i < numberOfLabels; i++) {
+        	desiredOutput[i][0] = labelInputStream.readUnsignedByte();
+            for (int r = 0; r < nRows; r++) {
+                for (int c = 0; c < nCols; c++) {
+                    trainingDataSet[i][r*nCols+c] = dataInputStream.readUnsignedByte();
+                }
+            }
+            
+        }
+	}
+
 }
